a69d276163c9f13d69416a0df8457b88d37cea3f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-07-23 20:38:34 +0545

    update ui

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            <p>Dashboard</p>
+        </div>
+    );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+const landingPage = () => {
+    return (
+        <div>
+            <h1 className="text-5xl font-bold"> ainepal </h1>
+        </div>
+    );
+};
+
+export default landingPage;
